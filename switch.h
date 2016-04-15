2cf3c2e1bac8cd9c3e9ac944be6121f4b26eba63
igek <igek@hawaii.edu>
2016-04-14 15:09:23 -1000

    Update switch.h
    Functioning switch.h

--- switch.h
+++ switch.h
@@ -2,21 +2,88 @@
  * switch.h
  */
 
-#define MAXENTRIES 100
-#include "queue.h"
-#include "table.h"
-#include "link.h"
+#define MAX_HOSTS 1000
+typedef struct switchPacket {
+    packetBuffer node;
+    struct switchPacket * next;
+    struct switchPacket * prev;
+} switchNode;
 
+typedef struct {
+    switchNode * head;
+    switchNode * tail;
+    int size;
+} Queue;
 
-typedef struct{ /* state of switch */
-	int switchid;			/* switch id */
-	packetBuffer sendPacketBuff;	/* send packet buffer */
-	packetBuffer rcvPacketBuff;	/* receive packet buffer */
-	LinkInfo linkin[NUMHOSTS];		/* Incoming communication link */
-	LinkInfo linkout[NUMHOSTS];		/* Outgoing communication link */
-	FwdTable Table[MAXENTRIES]
+typedef struct {                     /* State of switch */
+    int physid;                      /* Physical ID */
+    int netaddr;                    /* Switch Network Address */
+    int root;
+    int dist;
+    int parent;
+    
+    packetBuffer sendPacketBuff;    /* Send Packet Buffer */
+    packetBuffer rcvPacketBuff;     /* Recieved Packet Buffer */
+    
+    packetBuffer sendSwitchInfo;
+    packetBuffer rcvSwitchInfo;
+    
+    LinkInfo linkin;                /* Incoming Communication Links */
+    LinkInfo linkout;               /* Outgoing Communication Links */
+    
+    Queue packetQ;                  /* Packet Queue */
+    int FwdTable[MAX_HOSTS][3];      /* Forwarding Table */
+    
 } switchState;
 
-void switchMain(switchState *sstate);
+void switchMain(switchState * sstate, linkArrayType *linkArray, char *fileName);
+void switchInit(switchState * sstate, int physid);
+void push(Queue *Q, packetBuffer data);
+packetBuffer pop(Queue *Q);
 
-void switchInit(switchState *sstate, int physid);
+/* Functions for Queue */
+void switchInitQueue(Queue * switchQ) {
+    switchQ->size = 0;
+    switchQ->head = (switchNode*)malloc(sizeof(switchNode));
+    switchQ->tail = (switchNode*)malloc(sizeof(switchNode));
+    
+    switchQ->head->next = switchQ->tail;
+    switchQ->head->prev = NULL;
+    
+    switchQ->tail->prev = switchQ->head;
+    switchQ->tail->next = NULL;
+}
+
+void push(Queue *Q, packetBuffer data) {
+    switchNode * addNode = (switchNode*)malloc(sizeof(switchNode));
+    
+    /* Define the new node attributes */
+    addNode->node = data;
+    addNode->next = Q->head->next;
+    addNode->prev = Q->head;
+    
+    /* Change pointers */
+    Q->head->next->prev = addNode;
+    Q->head->next = addNode;
+    Q->size++;
+}
+
+packetBuffer pop(Queue *Q) {
+    switchNode * current;
+    switchNode * previous;
+    packetBuffer data;
+    
+    if(Q->size > 0) {
+        current = Q->tail->prev;
+        previous = current->prev;
+        
+        Q->tail->prev = previous;
+        previous->next = Q->tail;
+        data = current->node;
+        
+        free(current);
+        Q->size--;
+        
+        return data;
+    }
+}
