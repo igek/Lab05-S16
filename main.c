7227553cfb9a3d6aacbc8943e9f91ef504949301
igek <igek@hawaii.edu>
2016-04-14 15:05:41 -1000

    Update main.c
    Added switch creation to main() function.

--- main.c
+++ main.c
@@ -23,33 +23,54 @@
 void main()
 {
 hostState hstate;             /* The host's state */
+switchState sstate;
 linkArrayType linkArray;
 manLinkArrayType manLinkArray;
+
+char filename[200];
+
+/* Get topology configuration file */
+printf("Enter topology configuration filename: ");
+scanf("%s", filename);
+
+FILE *file = fopen(filename, "r");
+if(file == NULL) {
+  perror("File could not be opened.\n");
+  return;
+}
 
 pid_t pid;  /* Process id */
 int physid; /* Physical ID of host */
 int i;
+int j;
 int k;
+
+/* Read configuration file */
+char read[10];
+fscanf(file, "%d %d", &switches, &hosts);
+links = (hosts + switches - 1)*2;
+
+fclose(file);
 
 /* 
  * Create nonblocking (pipes) between manager and hosts 
  * assuming that hosts have physical IDs 0, 1, ... 
  */
-manLinkArray.numlinks = NUMHOSTS;
+manLinkArray.numlinks = hosts + switches;
 netCreateConnections(& manLinkArray);
 
 /* Create links between nodes but not setting their end nodes */
 
-linkArray.numlinks = NUMLINKS;
+linkArray.numlinks = links;
 netCreateLinks(& linkArray);
 
 /* Set the end nodes of the links */
 
-netSetNetworkTopology(& linkArray);
+netSetNetworkTopology(& linkArray, filename);
 
 /* Create nodes and spawn their own processes, one process per node */ 
 
-for (physid = 0; physid < NUMHOSTS; physid++) {
+for (physid = 0; physid < (hosts + switches); physid++) {
 
    pid = fork();
 
@@ -58,31 +79,44 @@ for (physid = 0; physid < NUMHOSTS; physid++) {
       return;
    }
    else if (pid == 0) { /* The child process -- a host node */
+      if(physid < switches) { // Switch
+        switchInit(&sstate, physid);
+        k = netHostOutLink(&linkArray, physid);
+        sstate.out = linkArray.link[k];
+        
+        k = netHostInLink(&linkArray, physid);
+        sstate.linkin = linkArray.link[k];
+        
+        netCloseHostOtherLinks(&linkArray, physid);
+        
+        switchMain(&sstate, &linkArray, filename);
+      } else if (physid < switches + hosts) { // Host
 
-      hostInit(&hstate, physid);              /* Initialize host's state */
+        hostInit(&hstate, physid);              /* Initialize host's state */
 
-      /* Initialize the connection to the manager */ 
-      hstate.manLink = manLinkArray.link[physid];
+        /* Initialize the connection to the manager */ 
+        hstate.manLink = manLinkArray.link[physid];
 
-      /* 
-       * Close all connections not connect to the host
-       * Also close the manager's side of connections to host
-       */
-      netCloseConnections(& manLinkArray, physid);
+        /* 
+         * Close all connections not connect to the host
+         * Also close the manager's side of connections to host
+         */
+        netCloseConnections(& manLinkArray, physid);
+  
+        /* Initialize the host's incident communication links */
 
-      /* Initialize the host's incident communication links */
+        k = netHostOutLink(&linkArray, physid); /* Host's outgoing link */
+        hstate.linkout = linkArray.link[k];
 
-      k = netHostOutLink(&linkArray, physid); /* Host's outgoing link */
-      hstate.linkout = linkArray.link[k];
+        k = netHostInLink(&linkArray, physid); /* Host's incoming link */
+        hstate.linkin = linkArray.link[k];
 
-      k = netHostInLink(&linkArray, physid); /* Host's incoming link */
-      hstate.linkin = linkArray.link[k];
-
-      /* Close all other links -- not connected to the host */
-      netCloseHostOtherLinks(& linkArray, physid);
+        /* Close all other links -- not connected to the host */
+        netCloseHostOtherLinks(& linkArray, physid);
 
       /* Go to the main loop of the host node */
       hostMain(&hstate);
+      }
    }  
 }
 
